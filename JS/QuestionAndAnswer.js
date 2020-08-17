1cf20c5682f27c430fb9f02b2463226ecfd23822
Artur Jakubowski <a.jakubowskipl@gmail.com>
2020-08-17 14:49:22 +0200

    added functions that put question and answer to html

--- JS/QuestionAndAnswer.js
+++ JS/QuestionAndAnswer.js
@@ -1,5 +1,6 @@
 let answerList = [];
 let questionList = [];
+let trueAnswers = ["Germany", "Thanedd Incident", "Euro"];
 
 questionList[0] = ["Mercedes is company from:"];
 questionList[1] = ["Dijksta's leg is broken because of:"];
@@ -9,8 +10,17 @@ answerList[0] = ["USA", "France", "Japan", "Germany"];
 answerList[1] = ["Drunk Accident", "Thanedd Incident", "Roof Collapse", "Bad Step"];
 answerList[2] = ["Euro", "Dollar", "Mark", "Rubel"];
 
-// document.getElementById("question").innerText = questionList[0];
-// document.getElementById("answer1").innerText = answerList[0][0];
-// document.getElementById("answer2").innerText = answerList[0][1];
-// document.getElementById("answer3").innerText = answerList[0][2];
-// document.getElementById("answer4").innerText = answerList[0][3];
+function putQuestionToHTML(questionListIndex) {
+    document.getElementById("question").innerText = questionList[questionListIndex];
+}
+
+function putAnswerToHTML(answerListIndex) {
+    document.getElementById("answer1").innerText = answerList[answerListIndex][0];
+    document.getElementById("answer2").innerText = answerList[answerListIndex][1];
+    document.getElementById("answer3").innerText = answerList[answerListIndex][2];
+    document.getElementById("answer4").innerText = answerList[answerListIndex][3];
+}
+
+function checkIfAnswerIsTrue(answer) {
+    return trueAnswers.includes(answer);
+}

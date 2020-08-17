bab2c5202c6d5b33f54d3cf4edcc2566081cf600
Artur Jakubowski <a.jakubowskipl@gmail.com>
2020-08-17 14:27:12 +0200

    Added base lists with questions and answers

---
+++ JS/QuestionAndAnswer.js
@@ -0,0 +1,16 @@
+let answerList = [];
+let questionList = [];
+
+questionList[0] = ["Mercedes is company from:"];
+questionList[1] = ["Dijksta's leg is broken because of:"];
+questionList[2] = ["What's the currency in France?"];
+
+answerList[0] = ["USA", "France", "Japan", "Germany"];
+answerList[1] = ["Drunk Accident", "Thanedd Incident", "Roof Collapse", "Bad Step"];
+answerList[2] = ["Euro", "Dollar", "Mark", "Rubel"];
+
+// document.getElementById("question").innerText = questionList[0];
+// document.getElementById("answer1").innerText = answerList[0][0];
+// document.getElementById("answer2").innerText = answerList[0][1];
+// document.getElementById("answer3").innerText = answerList[0][2];
+// document.getElementById("answer4").innerText = answerList[0][3];

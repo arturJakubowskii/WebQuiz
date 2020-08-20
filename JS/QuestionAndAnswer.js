068f2577cf090857c78b359562b907d94bc269a2
Michal Myczka <michalmorgoth@gmail.com>
2020-08-20 12:59:43 +0200

    a bit more of refactor and added new questions

--- JS/QuestionAndAnswer.js
+++ JS/QuestionAndAnswer.js
@@ -1,6 +1,6 @@
 let answerList = [];
 let questionList = [];
-let trueAnswers = ["Germany", "Thanedd Incident", "Euro","FC Liverpool","RPG"];
+let trueAnswers = ["Germany", "Thanedd Incident", "Euro","FC Liverpool","RPG","Oxygen","50m","Nile","Johnny Depp","Mount Everest"];
 let buttonIdVariable = "";
 let userAnswer = "";
 let testUserAnswer = false;
@@ -12,6 +12,11 @@ questionList[1] = "Dijksta's leg is broken because of:";
 questionList[2] = "What's the currency in France?";
 questionList[3] = "Which Team Won The Champions League In Year 2018/2019?";
 questionList[4] = "What Type Of Game Gothic Is?";
+questionList[5] = "What element does 'O' represent on the periodic table?";
+questionList[6] = "In meters, how long is an Olympic swimming pool?";
+questionList[7] = "What's the name of the river that runs through Egypt?";
+questionList[8] = "Who played Jack Sparrow in Pirates Of The Caribbean";
+questionList[9] = "What's the highest mountain in the world?";
 
 
 
@@ -20,6 +25,11 @@ answerList[1] = ["Drunk Accident", "Thanedd Incident", "Roof Collapse", "Bad Ste
 answerList[2] = ["Euro", "Dollar", "Mark", "Rubel"];
 answerList[3] = ["FC Liverpool", "Tottenham Hotspoor", "Intermediolan", "Paris Saint Germain"];
 answerList[4] = ["Shooter","RPG","Horror Game", "RTS"];
+answerList[5] = ["Oxygen","Carbon","Zinc","Copper"];
+answerList[6] = ["60m","40m","50m","30m"];
+answerList[7] = ["Yangtze","Amazon","Nile","Niger"];
+answerList[8] = ["Leonardo DiCaprio","Christian Bale","Brad Pitt","Johnny Depp"];
+answerList[9] = ["K2","Mount Everest","Makalu","Lhotse"];
 
 function putQuestionToHTML(questionListIndex) {
     document.getElementById("question").innerText = questionList[questionListIndex];
@@ -87,6 +97,26 @@ function mainGameStart() {
         case questionNumber = 5:
 
             runMainFunctions(4);
+            break;
+        case questionNumber = 6:
+
+            runMainFunctions(5);
+            break;
+        case questionNumber = 7:
+
+            runMainFunctions(6);
+            break;
+        case questionNumber = 8:
+
+            runMainFunctions(7);
+            break;
+        case questionNumber = 9:
+
+            runMainFunctions(8);
+            break;
+        case questionNumber = 10:
+
+            runMainFunctions(9);
             break;
     }
 }

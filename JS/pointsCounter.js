47ce11953d3b517764391f9c5ccb84c891cea6f8
Michal Myczka <michalmorgoth@gmail.com>
2020-08-20 15:16:12 +0200

    popup

--- JS/pointsCounter.js
+++ JS/pointsCounter.js
@@ -11,13 +11,17 @@ function addPoints() {
         counter();
     }
     else if (testUserAnswer === false){
+        Swal.fire(
+            'Wrong Choice!',
+            'No Points Earned!',
+            'error'
+        )
         onTimesUp();
-        alert("Wrong Answer!");
         if (questionNumber >= trueAnswers.length){
             localStorage.setItem("storageName",totalScore);
             document.location.href = 'endPage.html';
         }
-        startTimer();
+        startTimer()
     }
 }
 
@@ -25,8 +29,12 @@ function counter(){
     questionScore = score / timePassed;
     totalScore += Math.floor(questionScore);
     document.getElementById("points").innerHTML = "Your Points: " + totalScore;
+    Swal.fire(
+        'Good job!',
+        'You Guessed Correctly!',
+        'success'
+    )
     onTimesUp();
-    alert("Correct!");
     if (questionNumber >= trueAnswers.length){
         localStorage.setItem("storageName",totalScore);
         document.location.href = 'endPage.html';
